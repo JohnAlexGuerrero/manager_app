24a281d8ec8b301e63d9662ed76c2810c2512f0a
jalexguerrero <johnalexguerrero@gmail.com>
2023-09-14 17:51:42 -0500

    template shoppings

--- routes/provider.py
+++ routes/provider.py
@@ -7,11 +7,11 @@ provider = Blueprint('providers', __name__)
 
 @provider.route('/providers/new', methods=['GET'])
 def new_provider():
-    return render_template('shopping/new.html')
+    return render_template('provider/new.html')
 
 @provider.route('/providers/new', methods=['POST'])
 def add_provider():
-    name = [field for field in request.form.values()]
+    name = request.form['name']
     
     provider = Provider.create(name=name)
-    return redirect('/shopping')
+    return redirect('/shoppings/new')
--- routes/shopping.py
+++ routes/shopping.py
@@ -1,8 +1,26 @@
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, redirect, request
+
+from models.provider import Invoice, Provider
+from models.inventory import Product
 
 shopping = Blueprint('shoppings', __name__)
 
 @shopping.route('/shoppings')
 def home():
-    return render_template('shopping/index.html')
+    list_obj = Provider.select()
+     
+    return render_template('shopping/index.html', list_obj=list_obj, num_items=len(list_obj))
+
+@shopping.route('/shoppings/new', methods=['GET'])
+def new_invoice():
+    providers = Provider.select(Provider.name)
+    products = Product.select()
+    
+    return render_template('shopping/new.html', providers=providers, items=products)
+
+@shopping.route('/shoppings/new', methods=['POST'])
+def add_invoice():
+    for rs in request.form:
+        print(rs)
+    return redirect('/shoppings')
